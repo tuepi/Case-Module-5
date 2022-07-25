917d2e9568eff4988b9f5da0086d92f654ac7261
chuong0608 <chuongtep113@gmail.com>
2022-07-26 04:26:55 +0700

    Merge branch 'tue' of https://github.com/tuepi/Case-Module-5
    # Conflicts:
    #	src/app/app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
